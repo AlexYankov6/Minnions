79160f6f295e08bcd2eabbcd13ee7aa132ee5a20
pramataroff <milenpramatarov@gmail.com>
2024-04-11 08:36:38 +0300

    first commit

---
+++ minions/ComboItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace minions
+{
+    public class ComboItem
+    {
+     
+          public int Id {  get; set; }
+          public string Name { get; set; }
+        
+    }
+}
